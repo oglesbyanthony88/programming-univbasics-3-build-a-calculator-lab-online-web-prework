5ba0f0a334066be7dc19f91918761d84f2d2b31c
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-06 21:20:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  subt = num1 - num2
+  subt
 end
 
 def division(num1, num2)
-
+  div = num1 / num2
+  div
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  mult
 end
 
 def modulo(num1, num2)
-
+  modu = num1 % num2
+  modu
 end
 
 def square_root(num)
-
+  squ = num**
+  squ
 end
